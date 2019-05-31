fbb16495926d889deb6f14c5c6ec02a5940e20b5
CarlRWarren <cwarren@student.neumont.edu>
2019-05-31 09:01:38 -0600

    penguin animations with player controller

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,10 +15,10 @@ public class Player : MonoBehaviour
     void Update()
     {
         Vector3 translate = Vector3.zero;
-        translate.z = Input.GetAxis("Vertical");
-        translate.x =  Input.GetAxis("Horizontal");
+        translate.z = Input.GetAxis("Vertical") * 5.0f;
+        translate.x = Input.GetAxis("Horizontal") * 5.0f;
 
-        if (translate.x > 0.0f || translate.z > 0.0f)
+        if (translate.x != 0.0f || translate.z != 0.0f)
         {
             m_animator.SetBool("Walking", true);
         }
