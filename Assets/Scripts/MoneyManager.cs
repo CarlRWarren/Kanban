0304c9fc7c2214707aef3ed848c6448859552cd8
Neko Anstle <nekoanstle@gmail.com>
2019-06-03 09:10:00 -0600

    Adding Money Manager
    Money manager saves player money and keeps it even when game is closed and reopened

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoneyManager : MonoBehaviour
+{
+    public TextMeshProUGUI m_moneyText = null;
+
+    private int m_money = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_money = PlayerPrefs.GetInt("Money");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_moneyText.text = "$" + m_money.ToString("D5");
+        PlayerPrefs.SetInt("Money", m_money);
+    }
+
+    public void AddMoney()
+    {
+        m_money += 100;
+    }
+
+    public void LoseMoney()
+    {
+        m_money -= 100;
+    }
+}
