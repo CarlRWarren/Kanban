e47c26b79fc3121235be51e5910e28844359d6bd
GenerousTraveler <38059664+GenerousTraveler@users.noreply.github.com>
2019-06-04 09:36:41 -0600

    UIUpdate

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void LoadGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Jon");
+    }
+
+    public void OpenSettings()
+    {
+
+    }
+
+    public void OpenShop()
+    {
+
+    }
+}
