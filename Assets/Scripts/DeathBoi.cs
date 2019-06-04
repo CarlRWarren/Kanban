40fb116a9fbdaccc1102f28beaae08d0b4b9aa6f
QuickCoyote <36782326+QuickCoyote@users.noreply.github.com>
2019-06-03 18:03:02 -0600

    yeet

---
+++ Assets/Scripts/DeathBoi.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathBoi : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        other.GetComponent<PlayerScript>().Die();
+    }
+}
--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -5,26 +5,37 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour
 {
     float timer = 0.0f;
+    public bool inControl = true;
     void Update()
     {
-        timer += Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.W))
+        if (inControl)
         {
-            gameObject.GetComponentInChildren<Animator>().SetTrigger("Hop");
-            transform.rotation = new Quaternion(0, 0, 0, 0);
-            transform.Translate(Vector3.forward);
+            timer += Time.deltaTime;
+            if (Input.GetKeyDown(KeyCode.W))
+            {
+                gameObject.GetComponentInChildren<Animator>().SetTrigger("Hop");
+                transform.rotation = new Quaternion(0, 0, 0, 0);
+                transform.Translate(Vector3.forward);
+            }
+            if (Input.GetKeyDown(KeyCode.A))
+            {
+                gameObject.GetComponentInChildren<Animator>().SetTrigger("Hop");
+                transform.rotation = new Quaternion(0, -90, 0, 0);
+                transform.Translate(Vector3.right);
+            }
+            if (Input.GetKeyDown(KeyCode.D))
+            {
+                gameObject.GetComponentInChildren<Animator>().SetTrigger("Hop");
+                transform.rotation = new Quaternion(0, 90, 0, 0);
+                transform.Translate(Vector3.left);
+            }
         }
-        if (Input.GetKeyDown(KeyCode.A))
-        {
-            gameObject.GetComponentInChildren<Animator>().SetTrigger("Hop");
-            transform.rotation = new Quaternion(0, -90, 0, 0);
-            transform.Translate(Vector3.left);
-        }
-        if (Input.GetKeyDown(KeyCode.D))
-        {
-            gameObject.GetComponentInChildren<Animator>().SetTrigger("Hop");
-            transform.rotation = new Quaternion(0, 90, 0, 0);
-            transform.Translate(Vector3.right);
-        }
+    }
+
+    public void Die()
+    {
+        inControl = false;
+        transform.rotation = new Quaternion(90, 0, 0, 0);
+        //Do gameover stuffs
     }
 }
