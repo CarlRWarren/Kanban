4b0bc2b174071c5951ff8a67f1d33bce6d4d6c87
GenerousTraveler <38059664+GenerousTraveler@users.noreply.github.com>
2019-06-06 02:05:25 -0600

    Fireworks

---
+++ Assets/Scripts/Fireworks.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fireworks : MonoBehaviour
+{
+    [SerializeField] ParticleSystem[] m_particles;
+
+    private void Start()
+    {
+        StartCoroutine(StartFireWorks());
+    }
+
+    IEnumerator StartFireWorks()
+    {
+        while(true)
+        {
+            int index = (int)Mathf.Floor(Random.Range(0.0f, m_particles.Length));
+            m_particles[index].Emit(60);
+            Debug.Log("Boom!");
+            yield return new WaitForSeconds(Random.Range(0.0f, 1.0f) + 1.0f);
+        }
+    }
+}
